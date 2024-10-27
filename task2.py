2b66bcc121ea4328675287b46f1a5375ab25c414
Kamal Ali <kmali@mun.ca>
2024-10-26 18:41:56 -0600

    add missing minimze step. improve variable  naming and printing.
    answer seems still not correct

--- task2.py
+++ task2.py
@@ -2,42 +2,61 @@ from qiskit_optimization.problems import QuadraticProgram
 from qiskit_optimization.algorithms import CplexOptimizer,  GurobiOptimizer
 import numpy as np
 
-N = 10 # num of bins
-I = 10 # num of items
-B = 1 # weight of bin
+num_bins = 5 # num of bins
+num_items = 6 # num of items
+B = 10 # weight of bin
 
-weights = np.ones(I) * 0.49
-weights = np.random.rand(I)
+weights = np.random.randint(1, B/2, num_items)
 print(weights)
 
 # define a problem
 qp = QuadraticProgram()
-for j in range(N):
+for j in range(num_bins):
     qp.binary_var(f"y{j}")
 
-for i in range(I):
-    for j in range(N):
+for i in range(num_items):
+    for j in range(num_bins):
         qp.binary_var(f"x{i}{j}")
 
-for j in range(N):
-    constraint = {}
-    for i in range(I):
-        constraint[f"x{i}{j}"] = weights[i].item()
-    print('weight constraint', constraint)
-    qp.linear_constraint(constraint, "<=", 1)
+to_be_minimized = {}
+for j in range(num_bins):
+    to_be_minimized[f'y{j}'] = 1
+print('to_be_minimized', to_be_minimized)
+qp.minimize(linear=to_be_minimized)
 
-for i in range(I):
-    constraint = {}
-    for j in range(N):
-        constraint[f"x{i}{j}"] = 1
-    print('item in one bin constraint', constraint)
-    qp.linear_constraint(constraint, "=", 1)
+# weight constraint
+for j in range(num_bins):
+    weight_constraint = {}
+    for i in range(num_items):
+        weight_constraint[f"x{i}{j}"] = weights[i].item() # need to link x with y in a constraint?
+    print('weight constraint', weight_constraint)
+    qp.linear_constraint(weight_constraint, "<=", 10)
+
+# one item per bin constraint
+for i in range(num_items):
+    item_per_bin_constraint = {}
+    for j in range(num_bins):
+        item_per_bin_constraint[f"x{i}{j}"] = 1
+    print('item in one bin constraint', item_per_bin_constraint)
+    qp.linear_constraint(item_per_bin_constraint, "=", 1)
+
+# use at least one bin constraint
+min_one_bin_constraint = {}
+for j in range(num_bins):
+    min_one_bin_constraint[f'y{j}'] = 1
+qp.linear_constraint(min_one_bin_constraint, ">=", 1)
 
 cplex_result = CplexOptimizer().solve(qp)
 gurobi_result = GurobiOptimizer().solve(qp)
 
 print("cplex")
-print(cplex_result.prettyprint())
+for j in range(num_bins):
+    print(f'y{j}', cplex_result.variables_dict[f'y{j}'])
+
+for i in range(num_items):
+    for j in range(num_bins):
+        print(cplex_result.variables_dict[f"x{i}{j}"], end=' ')
+    print()
 print()
 print("gurobi")
 print(gurobi_result.prettyprint())
