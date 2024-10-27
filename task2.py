75cee0dc2c4d46cc750e15f7b7aba08da7660970
Kamal Ali <kmali@mun.ca>
2024-10-26 19:51:15 -0600

    working with gurobi!

--- task2.py
+++ task2.py
@@ -2,12 +2,12 @@ from qiskit_optimization.problems import QuadraticProgram
 from qiskit_optimization.algorithms import CplexOptimizer,  GurobiOptimizer
 import numpy as np
 
-num_bins = 5 # num of bins
+num_bins = 4  # num of bins
 num_items = 6 # num of items
 B = 10 # weight of bin
 
 weights = np.random.randint(1, B/2, num_items)
-print(weights)
+print('weights', weights)
 
 # define a problem
 qp = QuadraticProgram()
@@ -21,24 +21,21 @@ for i in range(num_items):
 to_be_minimized = {}
 for j in range(num_bins):
     to_be_minimized[f'y{j}'] = 1
-print('to_be_minimized', to_be_minimized)
 qp.minimize(linear=to_be_minimized)
 
 # weight constraint
 for j in range(num_bins):
     weight_constraint = {}
     for i in range(num_items):
-        weight_constraint[f"x{i}{j}"] = weights[i].item() # need to link x with y in a constraint?
-    print('weight constraint', weight_constraint)
-    qp.linear_constraint(weight_constraint, "<=", 10)
+        weight_constraint[(f"x{i}{j}", f'y{j}')] = weights[i].item() 
+    qp.quadratic_constraint(quadratic=weight_constraint, sense="<=", rhs=10)
 
-# one item per bin constraint
+# use all items constraint
 for i in range(num_items):
-    item_per_bin_constraint = {}
+    use_all_items_constraint = {}
     for j in range(num_bins):
-        item_per_bin_constraint[f"x{i}{j}"] = 1
-    print('item in one bin constraint', item_per_bin_constraint)
-    qp.linear_constraint(item_per_bin_constraint, "=", 1)
+        use_all_items_constraint[(f"x{i}{j}", f'y{j}')] = 1
+    qp.quadratic_constraint(quadratic=use_all_items_constraint, sense="=", rhs=1)
 
 # use at least one bin constraint
 min_one_bin_constraint = {}
@@ -46,17 +43,17 @@ for j in range(num_bins):
     min_one_bin_constraint[f'y{j}'] = 1
 qp.linear_constraint(min_one_bin_constraint, ">=", 1)
 
-cplex_result = CplexOptimizer().solve(qp)
-gurobi_result = GurobiOptimizer().solve(qp)
+results = {}
+# results['cplex'] = CplexOptimizer().solve(qp)
+results['gurobi'] = GurobiOptimizer().solve(qp)
 
-print("cplex")
-for j in range(num_bins):
-    print(f'y{j}', cplex_result.variables_dict[f'y{j}'])
-
-for i in range(num_items):
+for name, result in results.items():
+    print(name)
     for j in range(num_bins):
-        print(cplex_result.variables_dict[f"x{i}{j}"], end=' ')
+        print(f'y{j}', round(result.variables_dict[f'y{j}']))
+
+    for i in range(num_items):
+        for j in range(num_bins):
+            print(round(result.variables_dict[f"x{i}{j}"]), end=' ')
+        print()
     print()
-print()
-print("gurobi")
-print(gurobi_result.prettyprint())
