d1d069bc97f2dbea33d23ac7ad9f7756e6b66b45
Kamal Ali <kmali@mun.ca>
2024-11-07 17:27:05 -0600

    my latest

--- qaoa.py
+++ qaoa.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 import pennylane as qml
+import pennylane.numpy as npp
+
 
 shots = 500  # Number of samples used
 dev = qml.device("default.qubit", shots=shots)
@@ -40,7 +42,7 @@ for j in range(num_bins):
             
             Q_matrix[(i*num_bins+j)+num_bins][(i*num_bins+j)+num_bins] -= 1/4*penalty_2
             Q_matrix[(k*num_bins+j)+num_bins][(k*num_bins+j)+num_bins] -= 1/4*penalty_2
-            H += 1/4*penalty_2*scaled_weights[i]*scaled_weights[k]
+            # H += 1/4*penalty_2*scaled_weights[i]*scaled_weights[k]
 
 # constraint 2: use all items once constraint
 for i in range(num_items):
@@ -53,7 +55,7 @@ for i in range(num_items):
 
             Q_matrix[row][row] -= 1/2*penalty_1
             Q_matrix[col][col] -= 1/2*penalty_1
-            H += 1/2*penalty_1 
+            # H += 1/2*penalty_1 
 
 n_qubits = Q_matrix.shape[0]
 for row in range(n_qubits):
@@ -62,4 +64,21 @@ for row in range(n_qubits):
             H += Q_matrix[row][row] * qml.PauliZ(row)
         elif Q_matrix[row][col] != 0:
             H += Q_matrix[row][col] * qml.PauliZ(row) @ qml.PauliZ(col)
-print(H)
+# print(H)
+
+@qml.qnode(dev)
+def circuit(params):
+    for param, wire in zip(params, H.wires):
+        qml.RY(param, wires=wire)
+    return qml.sample()
+
+
+params = np.array([5 for _ in range(len(H.wires))])
+print(params)
+opt = qml.AdamOptimizer(stepsize=0.5)
+epochs = 200
+
+for epoch in range(epochs):
+    params = opt.step(circuit, params)
+    print(params)
+print(circuit(params))
